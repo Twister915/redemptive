dd0e3b05e1d9d8fd8348d541c25f9bd9a0a0e79f
Joey Sacchini <joey.sacchini264@gmail.com>
2016-05-28 12:55:49 -0400

    Changed up timeline so that it is much simpler now.

--- redemptive-core/src/main/java/tech/rayline/core/util/Timeline.java
+++ redemptive-core/src/main/java/tech/rayline/core/util/Timeline.java
@@ -2,6 +2,7 @@ package tech.rayline.core.util;
 
 import lombok.Data;
 import rx.Scheduler;
+import rx.Subscription;
 import rx.functions.Action0;
 
 import java.util.ArrayList;
@@ -12,39 +13,17 @@ import java.util.concurrent.TimeUnit;
 
 public final class Timeline {
     private final List<TimelineNode> nodes = new ArrayList<>();
-    private Scheduler.Worker worker;
 
     public static TimelineBuilder create() {
         return new TimelineBuilder();
     }
 
     public void addAt(Runnable runnable, long time, TimeUnit unit) {
-        ensureNotPlaying();
         nodes.add(new TimelineNode(runnable, unit.toMillis(time)));
     }
 
-    public void playLoop(final Scheduler scheduler) {
-        play(scheduler, new Action0() {
-            @Override
-            public void call() {
-                clearWorker();
-                playLoop(scheduler);
-            }
-        });
-    }
-
-    public void playOnce(Scheduler scheduler) {
-        play(scheduler, new Action0() {
-            @Override
-            public void call() {
-                clearWorker();
-            }
-        });
-    }
-
-    public void play(Scheduler scheduler, Action0 endHandler) {
-        ensureNotPlaying();
-        worker = scheduler.createWorker();
+    public Subscription play(Scheduler scheduler, Action0 endHandler) {
+        Scheduler.Worker worker = scheduler.createWorker();
         Collections.sort(nodes, new Comparator<TimelineNode>() {
             @Override
             public int compare(TimelineNode o1, TimelineNode o2) {
@@ -55,27 +34,7 @@ public final class Timeline {
             worker.schedule(node, node.msAfter, TimeUnit.MILLISECONDS);
         long endTime = nodes.get(nodes.size() - 1).msAfter;
         worker.schedule(endHandler, endTime, TimeUnit.MILLISECONDS);
-    }
-
-    public boolean isPlaying() {
-        return worker != null;
-    }
-
-    public void stop() {
-        if (!isPlaying())
-            throw new IllegalStateException("We're not currently playing this timeline!");
-
-        worker.unsubscribe();
-        clearWorker();
-    }
-
-    private void clearWorker() {
-        worker = null;
-    }
-
-    private void ensureNotPlaying() {
-        if (isPlaying())
-            throw new IllegalStateException("We're currently playing the timeline!");
+        return worker;
     }
 
     @Data public static final class TimelineNode implements Action0 {
